a64bc34a233a61739b53d2b775cd34dae8696d81
skantay <skantay03@gmail.com>
2024-02-19 03:41:03 +0600

    uploading

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -8,10 +8,6 @@ import (
 	"github.com/skantay/lets-go-further/internal/data"
 )
 
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
-}
-
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
@@ -31,4 +27,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	if err := app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
+}
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
