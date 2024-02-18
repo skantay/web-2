d1414c15d3dead80bf1f8ef09c257fd8e37ec974
skantay <skantay03@gmail.com>
2024-02-19 03:21:29 +0600

    mass upload

---
+++ cmd/api/movies.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/skantay/lets-go-further/internal/data"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create a new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "ASD",
+		Runtime:   123,
+		Genres:    []string{"Asd"},
+		Version:   1,
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
